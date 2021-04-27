a222db16bb8b9ac55132875773a6ab987ec421ec
Pawel-Kigler <73522977+Pawel-Kigler@users.noreply.github.com>
2021-04-27 11:06:27 +0200

    menu

--- assety/menu.php
+++ assety/menu.php
@@ -1,58 +1,58 @@
 
 <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler">
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            Github
+            >Github<
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
           </a>
           <a href="/pracownicy-org/pracownicy.php">
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            Pracownicy
+            >Pracownicy<
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
 
 
           </a>
           <a href="/pracownicy-org/organizacja.php">
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            Pracownicy i Organizacja
+            >Pracownicy i Organizacja<
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
 
 
           </a>
           <a href="/pracownicy-org/funkcje.php">
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            Funkcje Agregujące
+            >Funkcje Agregujące<
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
 
 
           </a>
           <a href="/pracownicy-org/dataczas.php">
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            Data i Czas
+            >Data i Czas<
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
 
 
           </a>
           <a href="/inne/daneDoBazy.php">
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            Dane Do Bazy
+            >Dane Do Bazy<
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
 
 
           </a>
           <a href="/biblioteka/biblioteka.php">
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            Książki
+            >Książki<
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
           </a>
             
             <a href="/flexbox/index.html">
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            Flexbox
+            >Flexbox<
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
 
             <a href="/relacje/relacje.php">
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            Relacje
+            >Relacje<
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
             </a>
             </a> 
@@ -62,32 +62,32 @@
           
             <a class="ars" href="../relacje/autor-tytul.php">
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            Autor-Tytul
+            >Autor-Tytul<
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
             </a>
             
             <a class="ars" href="../relacje/szkola.php">
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            Nauczyciel-Klasa
+            >Nauczyciel-Klasa<
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
 
 
             </a>
             <a class="ars" href="../relacje/firma.php">
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            Producent-Produkt
+            >Producent-Produkt<
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
 
 
             </a>
             <a class="ars" href="../relacje/warsztat.php">
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            Mechanik-Pojazd
+            >Mechanik-Pojazd<
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
             </a>
             <a class="ars" href="../relacje/pracownik.php">
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            Pracownik-Dzial
+            >Pracownik-Dzial<
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
             
             </a>
