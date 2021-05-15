59768a667412375ae7d883f904def08cc0d586bf
Pawel-Kigler <73522977+Pawel-Kigler@users.noreply.github.com>
2021-05-15 14:10:51 +0200

    dodanie 4 zadania

--- assety/menu.php
+++ assety/menu.php
@@ -85,6 +85,14 @@
             </a>
 
 
+            </a>
+          <a href="/d.php">
+            <img class="nav_icon" src="https://github.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/4.png"/>
+            >Film 4<
+            <img class="nav_icon" src="https://github.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/4.png"/>
+            </a>   
+
+
             <b>Wiele Do Wielu:</b>
             
           
