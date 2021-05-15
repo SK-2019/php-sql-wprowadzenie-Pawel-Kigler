c0dae859f4c2bd75eab13aad7dd26f6aed48b108
Pawel-Kigler <73522977+Pawel-Kigler@users.noreply.github.com>
2021-05-15 13:46:12 +0200

    zadanie

--- assety/menu.php
+++ assety/menu.php
@@ -66,8 +66,17 @@
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
             >Film 1<
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            </a>          
-                      
+            </a>   
+
+
+            </a>
+          <a href="/b.php">
+            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
+            >Film 2<
+            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
+            </a>   
+
+
             <b>Wiele Do Wielu:</b>
             
           
