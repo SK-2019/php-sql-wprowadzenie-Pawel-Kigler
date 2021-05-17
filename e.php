5acf23240c612e7bd7308e4839b3278fda04c99e
Pawel-Kigler <73522977+Pawel-Kigler@users.noreply.github.com>
2021-05-17 23:25:12 +0200

    Update e.php

--- e.php
+++ e.php
@@ -1,7 +1,7 @@
 <?php
 $q = mysqli_connect('mysql-pawelk.alwaysdata.net','pawelk','haslo123','pawelk_1') or die('Padl serwer');
-$wynik = mysqli_query($q,'select * from pracownicy');
+$wynik = mysqli_query($q,'select * from pracownicy') or die('Blad bazy danych');
 while ($txt = mysqli_fetch_assoc($wynik)){
-print_r($txt);
-}
+echo($txt['imie'].' '.$txt['nazwisko'].'<br>');
+} 
 ?>
