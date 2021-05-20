8748d7ba54bcbd790b658ca24dd52ef8ebf5dbd4
Pawel-Kigler <73522977+Pawel-Kigler@users.noreply.github.com>
2021-05-20 11:36:15 +0200

    Update e.php

--- e.php
+++ e.php
@@ -1,4 +1,5 @@
 <?php
+echo("<a href='index.php'> Powrót do strony głównej</a><li>");
 $q = mysqli_connect('mysql-pawelk.alwaysdata.net','pawelk','haslo123','pawelk_1') or die('Padl serwer');
 $wynik = mysqli_query($q,'select * from pracownicy') or die('Blad bazy danych');
 while ($txt = mysqli_fetch_assoc($wynik)){
