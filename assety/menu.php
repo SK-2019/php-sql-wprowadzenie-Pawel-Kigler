41e9f04e3d0ace384f44d03881f34d3d87c4fba6
Pawel-Kigler <73522977+Pawel-Kigler@users.noreply.github.com>
2021-06-12 19:58:03 +0200

    Update menu.php

--- assety/menu.php
+++ assety/menu.php
@@ -114,32 +114,6 @@
           
             <a class="ars" href="../relacje/autor-tytul.php">
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            >Autor-Tytul<
+            >Pilkarz-Klub<
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            </a>
-            
-            <a class="ars" href="../relacje/szkola.php">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            >Nauczyciel-Klasa<
-            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-
-
-            </a>
-            <a class="ars" href="../relacje/firma.php">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            >Producent-Produkt<
-            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-
-
-            </a>
-            <a class="ars" href="../relacje/warsztat.php">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            >Mechanik-Pojazd<
-            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            </a>
-            <a class="ars" href="../relacje/pracownik.php">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            >Pracownik-Dzial<
-            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Pawel-Kigler/main/like.png"/>
-            
             </a>
