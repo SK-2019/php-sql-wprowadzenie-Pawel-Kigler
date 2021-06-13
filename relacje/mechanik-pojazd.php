e292cde944146e451ca2e88253a0f35af32c86e7
Pawel-Kigler <73522977+Pawel-Kigler@users.noreply.github.com>
2021-06-13 18:01:12 +0200

    Update mechanik-pojazd.php

--- relacje/mechanik-pojazd.php
+++ relacje/mechanik-pojazd.php
@@ -1,10 +1,3 @@
-<?php
-
-        ini_set('display_errors', '1');
-        ini_set('display_startup_errors', '1');
-        error_reporting(E_ALL);
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -26,6 +19,10 @@
       <div class="item colorGreen">
 
 <?php
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
 require("../connect.php");
 
