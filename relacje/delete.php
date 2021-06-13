e3986d5b4f130af0d567cce125592403599e675f
Pawel-Kigler <73522977+Pawel-Kigler@users.noreply.github.com>
2021-06-13 19:09:20 +0200

    Create delete.php

---
+++ relacje/delete.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once("../connect.php");
+$sql = "DELETE  FROM '".$_POST['Tabela']."' WHERE '".$_POST['Kolumna']."'= '".$_POST['Wiersz']."'";
+
+echo $sql;
+
+if ($conn->query($sql) === TRUE) {
+  echo ("<h2>Usunięto pomyślnie</h2>");
+} 
+else {
+  echo "Błąd: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+?>
