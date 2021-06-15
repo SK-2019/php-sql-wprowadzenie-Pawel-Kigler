d2f87bd85100599dc4fc27eef15703fceb01ca5a
Pawel-Kigler <73522977+Pawel-Kigler@users.noreply.github.com>
2021-06-15 15:04:31 +0200

    test2

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Pawel Kigler</title>
+    <title>Pawel Kigler</title>
     <link rel="stylesheet" href="/stylecss/style.css"/>
   </head>
   <body>
